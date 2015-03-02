6030b5224cc02b6fa0674369a19cab6e4dc641d2
Dries Steenhouwer <steenhouwer.dries@gmail.com>
2015-03-02 16:04:43 +0100

    update filters

--- app/assets/javascripts/store/spree_filters.js
+++ app/assets/javascripts/store/spree_filters.js
@@ -1,7 +1,6 @@
 //= require_self
 $(document).ready(function(){
     $('.styled-checkbox input').on('change', function(){
-        $(this).closest('form').submit();
-        return false;
+      $(this).closest('form').submit();
     });
 });
